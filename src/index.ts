a002cc58fb97bc9574a0d7989098de57c8f2fa1d
P-Chan <hello@0x50.io>
2020-09-27 03:42:46 +0900

    feat: Add bot

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('stickr')
+import { App } from '@slack/bolt'
+import dotenv from 'dotenv'
+
+if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV == undefined) {
+  dotenv.config()
+}
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+})
+
+app.message('hello', async ({ message, say }) => {
+  await say(`Hey there <@${message.user}>!`)
+})
+;(async () => {
+  await app.start(process.env.PORT || 3000)
+
+  console.log('⚡️ Stickr is running!')
+})()
