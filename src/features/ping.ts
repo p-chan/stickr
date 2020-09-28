619b3360e1b338b0807d93ce1a2ec5e0fceb04a3
P-Chan <hello@0x50.io>
2020-09-28 12:08:37 +0900

    feat: Update feature name

--- src/features/ping.ts
+++ src/features/ping.ts
@@ -1,6 +1,6 @@
 import { App } from '@slack/bolt'
 
-export const PingFeature = (app: App) => {
+export const Ping = (app: App) => {
   app.event('app_mention', async ({ say }) => {
     await say('pong')
   })
