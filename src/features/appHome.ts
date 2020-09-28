3811664b060564409f1737653270e9c9d418fe0a
P-Chan <hello@0x50.io>
2020-09-28 21:18:06 +0900

    feat: Update AppHome feature
    Add save to local and upload to slack

--- src/features/appHome.ts
+++ src/features/appHome.ts
@@ -1,5 +1,11 @@
 import { App } from '@slack/bolt'
 import { PrismaClient } from '@prisma/client'
+import axios from 'axios'
+import cheerio from 'cheerio'
+import fetch from 'isomorphic-unfetch'
+import FormData from 'form-data'
+import fs from 'fs'
+import path from 'path'
 
 const prisma = new PrismaClient()
 
@@ -51,6 +57,38 @@ const newTokenInputBlock = {
   },
 }
 
+const newStickerButtonBlock = {
+  type: 'actions',
+  elements: [
+    {
+      type: 'button',
+      text: {
+        type: 'plain_text',
+        text: '新しいスタンプを追加する',
+      },
+      action_id: 'open_new_sticker_modal',
+    },
+  ],
+}
+
+const newStickerInputBlock = {
+  type: 'input',
+  block_id: 'primary',
+  label: {
+    type: 'plain_text',
+    text: 'スタンプの ID を入れてください',
+  },
+  element: {
+    type: 'plain_text_input',
+    placeholder: {
+      type: 'plain_text',
+      text: '123456',
+    },
+    action_id: 'product_id',
+    multiline: false,
+  },
+}
+
 export const AppHome = (app: App) => {
   /**
    * ホームを開いたとき
@@ -87,7 +125,11 @@ export const AppHome = (app: App) => {
         user_id: event.user,
         view: {
           type: 'home',
-          blocks: [createUserTokenSectionBlock({ hasToken: true, isVerify: isVerify.ok }), newTokenButtonBlock],
+          blocks: [
+            createUserTokenSectionBlock({ hasToken: true, isVerify: isVerify.ok }),
+            newTokenButtonBlock,
+            newStickerButtonBlock,
+          ],
         },
       })
     } catch (error) {
@@ -175,8 +217,169 @@ export const AppHome = (app: App) => {
         user_id: body.user.id,
         view: {
           type: 'home',
-          blocks: [createUserTokenSectionBlock({ hasToken: true, isVerify: false }), newTokenButtonBlock],
+          blocks: [
+            createUserTokenSectionBlock({ hasToken: true, isVerify: false }),
+            newTokenButtonBlock,
+            newStickerButtonBlock,
+          ],
         },
+      })
+    }
+  })
+
+  /**
+   * 新しいスタンプを追加するモーダルを開いたとき
+   */
+  app.action('open_new_sticker_modal', async ({ ack, body, client }) => {
+    ack()
+
+    await client.views.open({
+      trigger_id: (body as any).trigger_id,
+      view: {
+        type: 'modal',
+        callback_id: 'submit_new_sticker_modal',
+        title: {
+          type: 'plain_text',
+          text: '新しいスタンプを追加する',
+        },
+        blocks: [newStickerInputBlock],
+        submit: {
+          type: 'plain_text',
+          text: 'Submit',
+        },
+      },
+    })
+  })
+
+  /**
+   * 新しいトークンを設定するモーダルを submit したとき
+   */
+  app.view('submit_new_sticker_modal', async ({ ack, body, client }) => {
+    ack()
+
+    try {
+      const stickers: {
+        id: number
+        staticUrl: string
+      }[] = []
+      const productId: any = (body.view.state as any).values.primary.product_id.value
+      const temporaryDirectoryPath = path.resolve(process.cwd(), './tmp')
+
+      /**
+       * DM で開始を通知
+       */
+      await client.chat.postMessage({
+        channel: body.user.id,
+        text: 'スタンプの追加を開始します',
+      })
+
+      /**
+       * ユーザー情報を取得する
+       */
+      const user = await prisma.user
+        .findOne({
+          where: {
+            id_teamId: {
+              id: body.user.id,
+              teamId: body.team.id,
+            },
+          },
+        })
+        .then((user) => {
+          if (user == undefined) throw new Error('Can not found user')
+
+          return user
+        })
+
+      /**
+       * xoxs トークンを検証する
+       */
+      await client.auth.test({
+        token: user.xoxsToken,
+      })
+
+      /**
+       * LINE スタンプをスクレイピングする
+       */
+      const responseText = await fetch(`https://store.line.me/stickershop/product/${productId}/ja`).then(
+        async (response) => {
+          if (!response.ok) {
+            throw new Error('Sticker is not found')
+          }
+
+          return await response.text()
+        }
+      )
+
+      const $ = cheerio.load(responseText)
+
+      $('.FnStickerPreviewItem').each((_, element) => {
+        const json = JSON.parse(element.attribs['data-preview'])
+
+        stickers.push({
+          id: json.id,
+          staticUrl: json.staticUrl,
+        })
+      })
+
+      /**
+       * LINE スタンプをテンポラリーディレクトリに保存する
+       */
+      const savedStickers = await Promise.all(
+        stickers.map(async (sticker) => {
+          return await axios({
+            method: 'get',
+            url: sticker.staticUrl,
+            responseType: 'stream',
+          }).then((response) => {
+            const filePath = path.resolve(temporaryDirectoryPath, `${sticker.id}.png`)
+
+            response.data.pipe(fs.createWriteStream(filePath))
+
+            return {
+              id: sticker.id,
+              filePath: filePath,
+            }
+          })
+        })
+      )
+
+      /**
+       * LINE スタンプを emoji としてアップロードする
+       */
+      await Promise.all(
+        savedStickers.map(async (savedSticker) => {
+          const form: any = new FormData()
+          const file: any = fs.createReadStream(savedSticker.filePath)
+
+          form.append('mode', 'data')
+          form.append('name', `stickr_${savedSticker.id}`)
+          form.append('image', file)
+
+          const config = {
+            headers: {
+              Authorization: `Bearer ${user.xoxsToken}`,
+              ...form.getHeaders(),
+            },
+          }
+
+          await axios.post('https://slack.com/api/emoji.add', form, config)
+        })
+      )
+
+      /**
+       * DM で完了を通知
+       */
+      await client.chat.postMessage({
+        channel: body.user.id,
+        text: 'スタンプの追加が完了しました',
+      })
+    } catch (error) {
+      console.error(error)
+
+      await client.chat.postMessage({
+        channel: body.user.id,
+        text: '失敗しました...',
       })
     }
   })
